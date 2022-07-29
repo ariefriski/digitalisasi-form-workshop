686dafe956293f093fd8cddf19b33ed357ec784a
ariefriski <arief.riski11@gmail.com>
2022-07-29 10:44:35 +0700

    update draw io

--- application/views/v_form/form_perhitungan.php
+++ application/views/v_form/form_perhitungan.php
@@ -1,77 +1,96 @@
-<div class="row">
-<div class="col-md-2">
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="<?php echo base_url().'assets/media/photos/cbi-logo.png';?>" alt="logo-cbi" width="50" height="50" style="margin-left:3em"><br>
-<p style="text-align: center; font-size:12px;">PT. Century Batteries <br> Indonesia</p>
-</div>
-<div class="col-md-8">
-<h2 class="content-heading" style="text-align: center;">PERHITUNGAN COST PEMBUATAN PLAN</h2>
-</div>
-</div>
-<div class="row">
-<div class="col-md-6">
-<div class="form-material" style="margin-left:3em">
-    <input type="text" class="form-control" id="material-text" name="material-text" placeholder="">
-    <label for="material-text">Customer :</label>
-</div>
-<div class="form-material" style="margin-left:3em">
-    <input type="text" class="form-control" id="material-text" name="material-text" placeholder="">
-    <label for="material-text">Department :</label>
-</div>
-<div class="form-material" style="margin-left:3em">
-    <input type="text" class="form-control" id="material-text" name="material-text" placeholder="">
-    <label for="material-text">Part Name :</label>
-</div>
-<div class="form-material" style="margin-left:3em">
-    <input type="text" class="form-control" id="material-text" name="material-text" placeholder="">
-    <label for="material-text">Pekerjaan :</label>
-</div>  
-<div class="form-material" style="margin-left:3em">
-    <input type="text" class="form-control" id="material-text" name="material-text" placeholder="">
-    <label for="material-text">IN/OUT :</label>
-</div>  
-</div>
-<div class="col-md-4">
-<div class="form-material" style="margin-left:3em">
-    <input type="text" class="form-control" id="material-text" name="material-text" placeholder="">
-    <label for="material-text">Job Order :</label>
-</div>
-<div class="form-material" style="margin-left:3em">
-    <input type="text" class="form-control" id="material-text" name="material-text" placeholder="">
-    <label for="material-text">Drawing No :</label>
-</div>
-<div class="form-material" style="margin-left:3em">
-    <input type="text" class="form-control" id="material-text" name="material-text" placeholder="">
-    <label for="material-text">Revision :</label>
-</div>
-<div class="form-material" style="margin-left:3em">
-    <input type="text" class="form-control" id="material-text" name="material-text" placeholder="">
-    <label for="material-text">Receive Date/PIC :</label>
-</div>  
-<div class="form-material" style="margin-left:3em">
-    <input type="text" class="form-control" id="material-text" name="material-text" placeholder="">
-    <label for="material-text">Delivery :</label>
-</div>
-</div>
-</div>
-<div class="block">
-
-<div class="block-content">
-                            <table class="table table-bordered table-vcenter">
-                                <thead>
-                                    <tr>
-                                        <th class="text-left" style="width: 20%;">A. Part Name</th>
-                                        <th style="width: 15%;">Material</th>
-                                        <th class= "d-none d-sm-table-cell" style="width: 10%;">Dimensi</th>
-                                        <th style="width: 15%;">Masa</th>
-                                        <th style="width: 20px;">Unit Part (pcs)</th>
-                                        <th>Price/unit</th>
-                                        <th>Cost</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    
-                                </tbody>
-                            </table>
-                        </div>
-</div>
-            
+<style type="text/css">
+	table.tableizer-table {
+		font-size: 12px;
+		border: 1px solid #CCC; 
+		font-family: Arial, Helvetica, sans-serif;
+	} 
+	.tableizer-table td {
+		padding: 4px;
+		margin: 3px;
+		border: 1px solid #CCC;
+	}
+	.tableizer-table th {
+		background-color: #104E8B; 
+		color: #FFF;
+		font-weight: bold;
+	}
+</style>
+<table class="tableizer-table">
+<thead><tr class="tableizer-firstrow"><th></th><th>&nbsp;</th><th>&nbsp;</th><th>&nbsp;</th><th>&nbsp;</th><th>&nbsp;</th><th>&nbsp;</th><th>&nbsp;</th><th>&nbsp;</th><th>PLANNING</th></tr></thead><tbody>
+ <tr><td>PERHITUNGAN COST PEMBUATAN PART PLAN</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>Customer</td><td>:</td><td>-</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Job Order :</td><td>K-00-000</td></tr>
+ <tr><td>Dept.</td><td>:</td><td>-</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Drawing No. :</td><td>-</td></tr>
+ <tr><td>Part Name</td><td>:</td><td>-</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Revision :</td><td>-</td></tr>
+ <tr><td>Pekerjaan</td><td>:</td><td>-</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Received Date/PIC :</td><td>&nbsp;</td></tr>
+ <tr><td>IN/OUT</td><td>:</td><td>-</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Delivery :</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>A. PART NAME</td><td>&nbsp;</td><td>&nbsp;</td><td>Material</td><td>Dimensi volume [mm^3]</td><td>MASSA</td><td>&nbsp;</td><td>Unit part (pcs)</td><td>Price/unit</td><td>Cost</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>-</td><td>&nbsp;</td><td>&nbsp;</td><td>-</td><td>-</td><td>-</td><td>kg</td><td>-</td><td>&nbsp;</td><td> Rp-   </td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>A. TOTAL MATERIAL COST</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp-   </td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>B. DESIGN & PROGRAMMING</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Rate / H ( Rp )</td><td>&nbsp;</td><td>&nbsp;</td><td>Working Hours</td><td>&nbsp;</td><td>Cost</td></tr>
+ <tr><td>1</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>2</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>B. TOTAL MATERIAL COST</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp-   </td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>C. MACHINING PROCESS</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Rate / H ( Rp )</td><td>&nbsp;</td><td>Working Hours</td><td>&nbsp;</td><td>&nbsp;</td><td>Cost</td></tr>
+ <tr><td>1</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp- </td></tr>
+ <tr><td>2</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp- </td></tr>
+ <tr><td>3</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp- </td></tr>
+ <tr><td>4</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp- </td></tr>
+ <tr><td>5</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp- </td></tr>
+ <tr><td>6</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp- </td></tr>
+ <tr><td>7</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp- </td></tr>
+ <tr><td>8</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp- </td></tr>
+ <tr><td>9</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp- </td></tr>
+ <tr><td>C. TOTAL MATERIAL COST</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp-   </td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>D MIRROR POLISHING</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Rate / H ( US$ )</td><td>&nbsp;</td><td>Working Hours</td><td>&nbsp;</td><td>&nbsp;</td><td>Cost</td></tr>
+ <tr><td>1</td><td>Polishing</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp67.958 </td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp-   </td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>E. TEXTURE PROCESS</td><td>Inspection</td><td>&nbsp;</td><td>&nbsp;</td><td>Rate / H ( US$ )</td><td>&nbsp;</td><td>Working Hours</td><td>&nbsp;</td><td>&nbsp;</td><td>Cost</td></tr>
+ <tr><td>1</td><td>Inspection</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp67.958 </td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp-   </td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>F. Assy/ DIS-ASSY</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Rate / H ( US$ )</td><td>&nbsp;</td><td>Working Hours</td><td>&nbsp;</td><td>&nbsp;</td><td>Total Cost</td></tr>
+ <tr><td>1</td><td>Assy</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp67.958 </td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp-   </td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>G. TRIAL</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Working Hours</td><td>&nbsp;</td><td>&nbsp;</td><td>Total Cost</td></tr>
+ <tr><td>1</td><td>Trial</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp67.958 </td><td>&nbsp;</td><td>0,00</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp-   </td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td> $-   </td></tr>
+ <tr><td>H. MANAGEMENT FEE</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Working Hours</td><td>&nbsp;</td><td>&nbsp;</td><td>Total Cost</td></tr>
+ <tr><td>1</td><td>Management Fee </td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp-   </td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>TOTAL COST</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td> Rp-   </td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>REMARKS :</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>1 Material preparation 2 weeks if no stock available</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Prepared</td><td>&nbsp;</td><td>Checked</td><td>Approved</td></tr>
+ <tr><td>2 Cutting tools preparation 2 weeks if no stock available</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>3 Part 100% inspection</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Planner</td><td>&nbsp;</td><td>PRE Sect. Head</td><td>PRE Dept. Head</td></tr>
+ <tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td></td></tr>
+</tbody></table>
